394fe930d01660dca8f9b2d907a14d5946f34e6a
RIYA SINGH <rsriya383@gmail.com>
2026-09-04 14:31:37 -0400

    Create components.js

---
+++ dmv website/components.js
@@ -0,0 +1,36 @@
+const navigation = `
+<header class="navbar">
+
+    <a href="index.html" class="logo">
+
+        <span class="logo-mark">DMV</span>
+
+        <span class="logo-text">
+            <strong>PROTON THERAPY</strong>
+            <small>COLLABORATIVE</small>
+        </span>
+
+    </a>
+
+    <nav class="nav-links">
+
+        <a href="about.html">About</a>
+
+        <a href="centers.html">Our Centers</a>
+
+        <a href="symposium.html">Symposium</a>
+
+        <a href="resources.html">Resources</a>
+
+        <a href="news.html">News &amp; Events</a>
+
+        <a href="contact.html" class="nav-button">
+            Contact
+        </a>
+
+    </nav>
+
+</header>
+`;
+
+document.getElementById("navigation").innerHTML = navigation;
